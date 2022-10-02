d78a94fe53c53a1d7f14b22e575e92127f008ad7
Rupert Arzberger <44531288+RUARZ@users.noreply.github.com>
2022-10-02 19:14:07 +0200

    added attribute for inject marking. resolves #4

---
+++ source/src/InjeCtor.Core/Attribute/Inject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InjeCtor.Core.Attribute
+{
+    /// <summary>
+    /// Attribute to mark properties, methods, ... to inject types if found within mappings
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method)]
+    public class Inject : System.Attribute
+    {
+    }
+}
