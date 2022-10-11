25387b83376ec3f60efd5ab90b5353bcf06e9534
Rupert Arzberger <44531288+RUARZ@users.noreply.github.com>
2022-10-11 22:21:05 +0200

    Some refactorings and implementations of unit tests for InjeCtor and start for tests of Scope

---
+++ source/test/InjeCtor.Core.Test/TestClasses/InjeCtorTestClasses.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjeCtor.Core.Test.TestClasses
+{
+    class ClassWithoutMappedType { }
+
+    abstract class BaseClassForSingleton
+    {
+        public bool IsDisposed { get; set; }
+    }
+
+    abstract class BaseClassForNonDisposableSingleton
+    {
+        public bool IsDisposed { get; set; }
+    }
+
+    class NonDisposableSingleton : BaseClassForNonDisposableSingleton
+    {
+        public void Dispose()
+        {
+            IsDisposed = true;
+        }
+    }
+
+    class SingletonClass : BaseClassForSingleton, IDisposable
+    {
+        public static int CreationCounter { get; private set; }
+
+        public static void ResetCounter() => CreationCounter = 0;
+
+        public SingletonClass()
+        {
+            CreationCounter++;
+        }
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+        }
+    }
+}
